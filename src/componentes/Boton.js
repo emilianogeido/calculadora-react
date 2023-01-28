cb39977cd1d2725f9627214070a1cd7d6a445d44
emilianogeido <110432365+emilianogeido@users.noreply.github.com>
2023-01-28 17:53:13 -0300

    Completed calculator

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -10,7 +10,7 @@ function Boton(props) {
   return (
     <div
       className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}
-      onClick={() => props.manejarClic(props.children)}>
+      onClick={() => props.manejarClick(props.children)}>
       {props.children}
     </div>
   );
